54506289ad614ad58b5f0b08f19de537b2e2af3e
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-05-10 20:54:38 -0400

    Add state structure and started working on DFA structure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,44 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[deny(missing_docs)]
+
+pub mod state;
+use state::State;
+
+/// DFA represents a Deterministic Finite Automata.
+pub struct DFA {
+    /// A vector of all the states that are considered a final state.
+    final_states: Vec<i32>,
+
+    /// A vector of all of the states for this DFA.
+    states: Vec<State>,
+}
+
+pub struct DFABuilder {
+    /// A vector of all the states that are considered a final state.
+    final_states: Vec<i32>,
+
+    /// A vector of all of the states for this DFA.
+    states: Vec<State>,
+}
+
+impl DFABuilder {
+    pub fn new() -> Self {
+        Self {
+            final_states: Vec::new(),
+            states: Vec::new(),
+        }
+    }
+
+    /// Add the specified state id as a final state.
+    /// 
+    /// `state_id` the ID of the state that is considered a final state.
+    pub fn add_final_state(&mut self, state_id: i32) {
+        self.final_states.push(state_id);
+    }
+
+    /// Add the specified state to the DFA.
+    /// 
+    /// `state` the state to add to the DFA.
+    pub fn add_state(&mut self, state: State) {
+        self.states.push(state);
     }
 }
---
+++ src/state.rs
@@ -0,0 +1,68 @@
+//! A module for using States in a Deterministic Finite Automata (DFA).
+//! 
+//! # Example
+//! ```
+//! # use dfa::state::State;
+//! let mut state = State::new();
+//! state.add_transition('a', 1);
+//! state.add_transition('b', 2);
+//! 
+//! assert_eq!(state.num_transitions(), 2);
+//! ```
+
+use std::collections::HashMap;
+
+/// State
+#[derive(Debug, PartialEq, Eq)]
+pub struct State {
+    /// A map of all valid transitions from the state.
+    transitions: HashMap<char, i32>,
+}
+
+impl State {
+    /// Construct a new state with no initial transitions.
+    pub fn new() -> Self {
+        Self {
+            transitions: HashMap::new(),
+        }
+    }
+
+    /// Add the specified transition to this state.
+    /// 
+    /// No validation is made that `new_state` exists until after
+    /// all of the states have been added to the DFA.
+    pub fn add_transition(&mut self, w: char, new_state: i32) {
+        self.transitions.insert(w, new_state);
+    }
+
+    /// Return the transition from this state for the specified input.
+    pub fn transition_for(&self, w: char) -> Option<&i32> {
+        self.transitions.get(&w)
+    }
+
+    /// Return the number of transitions from this state.
+    pub fn num_transitions(&self) -> usize {
+        self.transitions.len()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn construct_state_initial() {
+        let initial = State::new();
+
+        assert_eq!(initial.num_transitions(), 0);
+    }
+
+    #[test]
+    fn constrct_state_with_transitions() {
+        let mut initial = State::new();
+        initial.add_transition('a', 1);
+        initial.add_transition('b', 2);
+
+        assert_eq!(initial.num_transitions(), 2);
+    }
+}
